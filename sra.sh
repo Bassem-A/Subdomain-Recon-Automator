3f71123df779d895bffe400c65a73c70491e745e
Bassem-A <73930604+Bassem-A@users.noreply.github.com>
2022-01-09 07:33:49 +0300

    updated

--- sra.sh
+++ sra.sh
@@ -6,7 +6,7 @@ while read target; do
 sublist3r -d $target -o tmp/sublister_$target.txt&>/dev/null&
 amass enum -active -d $target -o tmp/amass_$target.txt&>/dev/null&
 assetfinder $target &> tmp/assFinder_$target.txt&
-~/tools/subbrute/subbrute.py $target &> tmp/bruted_$target.txt&
+subbrute $target &> tmp/bruted_$target.txt&
 gobuster dns -d $target -w ~/Documents/subdomains/medium.txt -o tmp/brute_go_$target.txt&>/dev/null&
 
 #last(NO GOOD FORMAT)
